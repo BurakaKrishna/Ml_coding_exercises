1140cce0cfe35ac21e979844788a04182ac49c94
buraka <buraka@Apples-MacBook-Air.local>
2022-03-03 21:37:18 +0530

    Numpy basics

--- linear_regression.py
+++ linear_regression.py
@@ -21,8 +21,15 @@ def train_model(X,y):
 	weights = np.dot(inv(np.dot(X.T,X)),np.dot(X.T,y))
 	return weights
 
+# Does this work only with np.dot or np.outer not np.inner or np.matmul?
+# np.outer and np.inner are for vectors
+# outer : out[i, j] = a[i] * b[j]
+# inner : If a and b are nonscalar, their last dimensions must match,
+# inner : If a and b are both scalars or both 1-D arrays then a scalar is returned; otherwise an array is returned. out.shape = (*a.shape[:-1], *b.shape[:-1])
+# np.dot and np.matmul are for matrices
 
-# example
+
+# example data from https://cmdlinetips.com/2020/03/linear-regression-using-matrix-multiplication-in-python-using-numpy/
 import pandas as pd
 data_url = 'https://raw.githubusercontent.com/cmdlinetips/data/master/cars.tsv'
 cars = pd.read_csv(data_url, sep="\t")
@@ -39,3 +46,10 @@ y = np.array(y)
 coefficients = train_model(X_mat,y)
 print(coefficients)
 # [8.28390564 0.16556757] supposed to get
+
+# yet to do
+# gradient descent
+
+
+# What about multi feature linear regression, how does that work?
+
